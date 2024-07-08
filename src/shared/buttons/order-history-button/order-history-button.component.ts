a902ff0918b11056eff62663b5d9f0404255e58e
Ori Amzallag <oria@moveo.co.il>
2024-07-08 11:36:57 +0300

    order history shared button

---
+++ src/shared/buttons/order-history-button/order-history-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-history-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-history-button.component.html',
+  styleUrl: './order-history-button.component.scss'
+})
+export class OrderHistoryButtonComponent {
+
+}
