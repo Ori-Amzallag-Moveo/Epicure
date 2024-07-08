b10f3a7582eb298dba9607c856121aac895b63f6
Ori Amzallag <oria@moveo.co.il>
2024-07-08 11:37:31 +0300

    header component ipdate

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,35 @@
-import { Component } from '@angular/core';
-import { SearchBarComponent } from '../../shared/components/search-bar/search-bar.component';
+import { Component, OnInit } from '@angular/core';
+import { CartComponent } from '../../shared/components/cart/cart.component';
+import { CartService } from '../../shared/components/cart/cart.service';
+import { HeaderService } from './header.service';
+import { MobileNavbarComponent } from '../../shared/components/mobile-navbar/mobile-navbar.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [SearchBarComponent],
+  imports: [CartComponent, MobileNavbarComponent],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  navBarMobile: boolean = false;
 
+  constructor(
+    private cartService: CartService,
+    private headerService: HeaderService
+  ) {}
+
+  ngOnInit() {
+    this.headerService.navbarVisible$.subscribe((isVisible) => {
+      this.navBarMobile = isVisible;
+    });
+  }
+
+  onToggleCart() {
+    this.cartService.toggleCart();
+  }
+
+  onToggleNavbar() {
+    this.headerService.toggleNavbar();
+  }
 }
