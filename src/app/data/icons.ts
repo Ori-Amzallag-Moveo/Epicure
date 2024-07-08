83e55b14a565262783212d02043578071918b23a
Ori Amzallag <oria@moveo.co.il>
2024-07-08 11:10:04 +0300

    dummy data

---
+++ src/app/data/icons.ts
@@ -0,0 +1,5 @@
+export const icons = [
+    { name: 'spicy', img: 'assets/spicy.svg' },
+    { name: 'vegetarian', img: 'assets/vegeterian.svg' },
+    { name: 'vegan', img: 'assets/vegan.svg' }
+  ];
---
+++ src/app/data/signatureDishes.ts
@@ -0,0 +1,30 @@
+export const signatureDishes = [
+    {
+      name: 'Pad Ki Mao',
+      imageSrc: 'assets/pad-ki-mao.jpg',
+      ingredients: 'Shrimps, Glass Noodles, Kemiri Nuts, Shallots, Lemon Grass, Magic Chili Brown Coconut',
+      icons: [
+        { src: 'assets/spicy.svg', alt: 'Spicy' }
+      ],
+      price: 88
+    },
+    {
+      name: 'Garbanzo Frito',
+      imageSrc: 'assets/garbanzo-frito.jpg',
+      ingredients: 'Polenta fingers, veal cheek, magic chili cured lemon cream, yellow laksa',
+      icons: [
+        { src: 'assets/spicy.svg', alt: 'Spicy' },
+      ],
+      price: 79
+    },
+    {
+      name: 'Smoked Pizza',
+      imageSrc: 'assets/smoked-pizza.jpg',
+      ingredients: 'Basil dough, cashew "butter", demi-glace, bison & radish',
+      icons: [
+        { src: 'assets/vegan.svg', alt: 'Vegetarian' }
+      ],
+      price: 65
+    }
+  ];
+  
