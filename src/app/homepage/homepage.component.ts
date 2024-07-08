5edeff9e189d6b423c4bd7e29fd16ca71f8ce023
Ori Amzallag <oria@moveo.co.il>
2024-07-08 11:39:12 +0300

    update homepage on init and footer

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { PopularRestaurantsComponent } from './popular-restaurants/popular-restaurants.component';
 import { SignatureDishesComponent } from './signature-dishes/signature-dishes.component';
 import { GenericCardComponent } from '../../shared/components/cards/generic-card/generic-card.component';
 import { IconMeaningComponent } from './icon-meaning/icon-meaning.component';
 import { WeekChefComponent } from './week-chef/week-chef.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from '../footer/footer.component';
+import { CartService } from '../../shared/components/cart/cart.service';
 
 @Component({
   selector: 'app-homepage',
@@ -16,8 +19,32 @@ import { WeekChefComponent } from './week-chef/week-chef.component';
     GenericCardComponent,
     IconMeaningComponent,
     WeekChefComponent,
+    AboutComponent,
+    FooterComponent,
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss',
 })
-export class HomepageComponent {}
+
+export class HomepageComponent implements OnInit {
+  cartItems: any[] = [];
+  cartIsEmpty: boolean = true;
+  showCart: boolean = false;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.cartItems$.subscribe(items => {
+      this.cartItems = items;
+      this.cartIsEmpty = items.length === 0;
+    });
+
+    this.cartService.showCart$.subscribe(show => {
+      this.showCart = show;
+    });
+  }
+
+  toggleCart() {
+    this.cartService.toggleCart();
+  }
+}
