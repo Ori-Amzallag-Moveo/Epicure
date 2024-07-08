63f4f5f343c583eb827571212fe928ae6cb9bba5
Ori Amzallag <oria@moveo.co.il>
2024-07-08 10:56:48 +0300

    restaurants-button generic

---
+++ src/shared/buttons/restaurants-button/restaurants-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurants-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurants-button.component.html',
+  styleUrl: './restaurants-button.component.scss'
+})
+export class RestaurantsButtonComponent {
+
+}
