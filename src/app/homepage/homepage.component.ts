c91a40e2b4616c968961081125c713e8b364f10e
Ori Amzallag <oria@moveo.co.il>
2024-07-08 11:13:50 +0300

    homepage update

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from './hero/hero.component';
 import { PopularRestaurantsComponent } from "./popular-restaurants/popular-restaurants.component";
+import { SignatureDishesComponent } from './signature-dishes/signature-dishes.component';
+import { GenericCardComponent } from '../../shared/components/cards/generic-card/generic-card.component';
+import { IconMeaningComponent } from './icon-meaning/icon-meaning.component';
 
 
 
@@ -10,6 +13,9 @@ import { PopularRestaurantsComponent } from "./popular-restaurants/popular-resta
   imports: [
     HeroComponent,
     PopularRestaurantsComponent,
+    SignatureDishesComponent,
+    GenericCardComponent,
+    IconMeaningComponent,
 
   ],
   templateUrl: './homepage.component.html',
