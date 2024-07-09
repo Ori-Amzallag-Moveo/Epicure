d498a29d8ebae0487a4964ede7899a51a4103559
Ori Amzallag <oria@moveo.co.il>
2024-07-09 11:32:02 +0300

    prettier

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,4 +8,15 @@ import { SearchBarComponent } from '../../shared/components/search-bar/search-ba
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
+<<<<<<< HEAD
+export class HeaderComponent {
+  headerTabs = ['Restaurants', 'Chefs'];
+  toolbarIcons = [
+    { name: 'search', imgsrc: 'assets/hero-logos/search.svg' },
+    { name: 'client', imgsrc: 'assets/logos/navbar-logos/client.svg' },
+    { name: 'bag', imgsrc: 'assets/logos/navbar-logos/bag.svg' },
+  ];
+}
+=======
 export class HeaderComponent {}
+>>>>>>> e96d977 (prettier)
