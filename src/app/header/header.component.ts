c65339712b60e97d2924a7fb31227785c831ea92
Ori Amzallag <oria@moveo.co.il>
2024-07-12 01:24:28 +0300

    implment of for loops for tabs

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,13 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  headerTabs = ['Restaurants', 'Chefs'];
+  toolbarIcons = [
+    { name: 'search', imgsrc: 'assets/hero-logos/search.svg' },
+    { name: 'client', imgsrc: 'assets/logos/navbar-logos/client.svg' },
+    { name: 'bag', imgsrc: 'assets/logos/navbar-logos/bag.svg' },
+  ];
 }
