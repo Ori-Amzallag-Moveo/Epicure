fcb26508118f21572a29cc51f7a585e849a63474
Ori Amzallag <oria@moveo.co.il>
2024-07-15 16:45:42 +0300

    fix

--- src/app/pages/restaurants/map-view/map-view.component.ts
+++ src/app/pages/restaurants/map-view/map-view.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { restaurantsData } from '../../../data/restaurantsData';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-map-view',
   standalone: true,
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.scss'],
+  imports: [GoogleMapsModule],
 })
 export class MapViewComponent implements OnInit {
   center: google.maps.LatLngLiteral = { lat: 32.0853, lng: 34.7818 };
