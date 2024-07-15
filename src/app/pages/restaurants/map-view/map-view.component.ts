80fbdb95b087d7c154917b6c45749c7e904aa3d9
Ori Amzallag <oria@moveo.co.il>
2024-07-15 16:43:25 +0300

    google update

--- src/app/pages/restaurants/map-view/map-view.component.ts
+++ src/app/pages/restaurants/map-view/map-view.component.ts
@@ -1,7 +1,5 @@
-import { Component, AfterViewInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { restaurantsData } from '../../../data/restaurantsData';
-import { GoogleMapsLoaderService } from './google-maps-loader.service';
-import { PopularRestaurant } from '../../../models/popularRestaurants.model';
 
 @Component({
   selector: 'app-map-view',
@@ -9,60 +7,19 @@ import { PopularRestaurant } from '../../../models/popularRestaurants.model';
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.scss'],
 })
-export class MapViewComponent implements AfterViewInit {
-  constructor(private googleMapsLoader: GoogleMapsLoaderService) {}
+export class MapViewComponent implements OnInit {
+  center: google.maps.LatLngLiteral = { lat: 32.0853, lng: 34.7818 };
+  zoom = 12;
+  markerPositions: google.maps.LatLngLiteral[] = [];
 
-  ngAfterViewInit() {
-    this.googleMapsLoader
-      .load()
-      .then(() => {
-        this.initMap();
-      })
-      .catch((error) => {
-        console.error('Error loading Google Maps API:', error);
-      });
+  ngOnInit() {
+    this.loadMarkers();
   }
 
-  initMap() {
-    const mapOptions: google.maps.MapOptions = {
-      center: { lat: 32.0853, lng: 34.7818 },
-      zoom: 12,
-    };
-
-    const map = new google.maps.Map(
-      document.getElementById('map') as HTMLElement,
-      mapOptions
-    );
-
-    if (map) {
-      console.log('Map initialized successfully in Tel Aviv');
-      this.addRestaurantMarkers(map);
-    }
-  }
-
-  addRestaurantMarkers(map: google.maps.Map) {
-    restaurantsData.forEach((restaurant: PopularRestaurant) => {
-      if (restaurant.latitude && restaurant.longitude) {
-        const marker = new google.maps.Marker({
-          position: { lat: restaurant.latitude, lng: restaurant.longitude },
-          map: map,
-          title: restaurant.name,
-        });
-
-        const infoWindow = new google.maps.InfoWindow({
-          content: `
-            <div>
-              <h3>${restaurant.name}</h3>
-              <p>Chef: ${restaurant.chef}</p>
-              <img src="${restaurant.rating}" alt="Rating" />
-            </div>
-          `,
-        });
-
-        marker.addListener('click', () => {
-          infoWindow.open(map, marker);
-        });
-      }
-    });
+  loadMarkers() {
+    this.markerPositions = restaurantsData.map(restaurant => ({
+      lat: restaurant.latitude,
+      lng: restaurant.longitude,
+    }));
   }
 }
