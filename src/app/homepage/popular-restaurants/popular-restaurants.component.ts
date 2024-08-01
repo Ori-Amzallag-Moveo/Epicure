4292856b0eef69b1b616f6bd49f20c3a3e8dd06f
Ori Amzallag <oria@moveo.co.il>
2024-08-01 13:37:02 +0300

    service semonstarte

--- src/app/homepage/popular-restaurants/popular-restaurants.component.ts
+++ src/app/homepage/popular-restaurants/popular-restaurants.component.ts
@@ -1,26 +1,32 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { GenericCardComponent } from '../../../shared/components/cards/generic-card/generic-card.component';
 import { RestaurantsButtonComponent } from '../../../shared/buttons/restaurants-button/restaurants-button.component';
 import { CommonModule } from '@angular/common';
-import { popularRestaurants } from '../../data/popularRestaurants';
-import { breakpoints } from '../../data/breakpoints';
+import { breakpointsData } from '../../data/breakpointsData';
+import { Restaurant } from '../../models/restaurant.model';
+import { PopularRestaurantsService } from './popular-restaurants.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-popular-restaurants',
   standalone: true,
-  imports: [
-    GenericCardComponent,
-    RestaurantsButtonComponent,
-    CommonModule,
-
-  ],
+  imports: [GenericCardComponent, RestaurantsButtonComponent, CommonModule, HttpClientModule],
   templateUrl: './popular-restaurants.component.html',
-  styleUrl: './popular-restaurants.component.scss',
+  styleUrls: ['./popular-restaurants.component.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 
 })
-export class PopularRestaurantsComponent {
-  restaurants = popularRestaurantsData;
+export class PopularRestaurantsComponent implements OnInit {
+  restaurants: Restaurant[] = [];
+  breakpoints = breakpointsData;
 
-  breakpoints = breakpoints;
+  constructor(private popularRestaurantsService: PopularRestaurantsService) {}
+
+  ngOnInit() {
+    this.popularRestaurantsService.fetchRestaurants()
+      .pipe(tap((data: Restaurant[]) => this.restaurants = data))
+      .subscribe();
+  }
 }
