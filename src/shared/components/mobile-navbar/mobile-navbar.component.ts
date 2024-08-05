e36ee54b423b4718b9e6f6758c455e4b9f996c07
Ori Amzallag <oria@moveo.co.il>
2024-08-05 10:50:58 +0300

    minor fixes

--- src/shared/components/mobile-navbar/mobile-navbar.component.ts
+++ src/shared/components/mobile-navbar/mobile-navbar.component.ts
@@ -2,16 +2,23 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../../app/footer/footer.component';
 import { HeaderService } from '../../../app/header/header.service';
 import { Router } from '@angular/router'; 
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-mobile-navbar',
   standalone: true,
   templateUrl: './mobile-navbar.component.html',
-  styleUrl: './mobile-navbar.component.scss',
-  imports: [FooterComponent],
+  styleUrls: ['./mobile-navbar.component.scss'],
+  imports: [FooterComponent,CommonModule],
 })
 export class MobileNavbarComponent {
-  constructor(private headerService: HeaderService, private router: Router) {}
+  navBarMobile: boolean = false;
+
+  constructor(private headerService: HeaderService, private router: Router) {
+    this.headerService.navbarVisible$.subscribe(isVisible => {
+      this.navBarMobile = isVisible;
+    });
+  }
 
   onClosing() {
     this.headerService.toggleNavbar();
